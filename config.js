d19ff68842f103feefb03362e820b65f5f811a91
wolfjoker77 <wolfjoker234@gmail.com>
2025-08-31 03:33:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+// CC KILLER BOT - https://t.me/BLACKHATMONKCHAT
+
+module.exports = {
+  numberOfRegisterInOneTime: 6,  // CC KILLER BOT - https://t.me/BLACKHATMONKCHAT
+  // no of registration want to make in one command.
+
+  domains: ["lamoshitt.com", "ratvirus.shop"], // CC KILLER BOT - https://t.me/BLACKHATMONKCHAT
+  // names of domain for the emails.
+
+  oneTimeMailGen: 10 * 100000,
+
+  telegramBotToken: "7985829393:AAHsiB9fCV7pLNwlvo4UNH6rwyQSyZgWFiE", // CC KILLER BOT - https://t.me/BLACKHATMONKCHAT
+  // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
+
+  adminNamesForTelegramBot: ["@Nikboss9"], // CC KILLER BOT - https://t.me/BLACKHATMONKCHAT
+  // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+
+  authorisedUserForTelegramBot: ["@Nikboss9"], // CC KILLER BOT - https://t.me/BLACKHATMONKCHAT
+  // username or id of the user who can use the bot.
+};
+
+// CC KILLER BOT - https://t.me/BLACKHATMONKCHAT
